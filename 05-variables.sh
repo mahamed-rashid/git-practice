c20383e41463f514fb3ec80eed1f2191f07b0d96
mahamed-rashid <mahamed.rashid@gmail.com>
2024-08-25 12:22:46 -0400

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+echo "Please enter your Username:"
+
+read username
+
+echo "the username is: $username"
+
+echo "Please enter your Username:"
+
+read -s password
+
+echo "the username is: $password"
