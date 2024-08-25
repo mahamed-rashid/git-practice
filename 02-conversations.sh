c36fbe222ec655e4b8e99e300077edac39e1b949
mahamed-rashid <mahamed.rashid@gmail.com>
2024-08-24 22:20:47 -0400

    added conversations script

---
+++ 02-conversations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Rashid: Hi Osman, How are you ?"
+echo "Osman: I'm good. How about you?"
+echo "Rashid: I'm good too. what's going on?"
+echo "Osman: Are you learning DevOps?"
+echo "Rashid: yes"
