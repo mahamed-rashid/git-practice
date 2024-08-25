c3a3758f6cc892f5efe7c4825dfb30c9d5e54a68
mahamed-rashid <mahamed.rashid@gmail.com>
2024-08-24 22:55:24 -0400

    variables

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1: Hi Osman, How are you ?"
+echo "$person2: I'm good. How about you?"
+echo "$person1: I'm good too. what's going on?"
+echo "$person2: Are you learning DevOps?"
+echo "$person1: yes"
