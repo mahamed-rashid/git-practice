47202f04f1837e4f3ab348def9a25e4ed9453e2b
mahamed-rashid <mahamed.rashid@gmail.com>
2024-08-26 15:22:15 -0400

    learning about commands

---
+++ 09-special-vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "all args entered $@"
+echo "no of args entered $#"
+echo "Script name executed now $0"
+echo "current directory $PWD"
+echo "home directory for current logged in user $HOME"
+echo "PID of script executing now $$"
